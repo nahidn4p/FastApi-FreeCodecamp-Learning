bb046e7c39db827d19e04bff958c61b4ae32f0cb
nahidn4p <nahidn4p@gmail.com>
2024-11-26 16:44:54 +0600

    modified:   app/database.py 	modified:   app/main.py 	modified:   app/models.py 	modified:   requirements.txt

--- app/main.py
+++ app/main.py
@@ -3,10 +3,23 @@ from random import randrange
 from . import models
 from .database import engine
 from .routers import post,user,auth,vote
-
-models.Base.metadata.create_all(bind=engine)
+from  fastapi.middleware.cors import CORSMiddleware
+#models.Base.metadata.create_all(bind=engine)
 
 app=FastAPI()
+
+origins=["*"] #allowed domain to talk to my api .ex: "http://www.google.com"
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"], # '*' means allow all
+    allow_headers=["*"],
+)
+
+
+
 
 app.include_router(post.router)
 app.include_router(user.router)
